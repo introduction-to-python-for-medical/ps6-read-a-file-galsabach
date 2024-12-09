8c980723bd8b724f79d03e7287bb74d358ab2018
galsabach <galsabach66@gmail.com>
2024-12-09 17:44:12 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,14 +1,14 @@
 def create_codon_dict(file_path):
-file_path = "data/codons.txt"
-    file = open(file_path)
-    rows = file.readlines()
-    file.close()
+  file_path = "data/codons.txt"
+  file = open(file_path)
+  rows = file.readlines()
+  file.close()
 
-    dic = {}
-    for row in rows:
-      row = row.strip().split('\t')
-      codon = row[0]
-      amino_acid = row[2]
-      dic[codon] = amino_acid
-    return dic
+  dic = {}
+  for row in rows:
+    row = row.strip().split('\t')
+    codon = row[0]
+    amino_acid = row[2]
+    dic[codon] = amino_acid
+  return dic
 
